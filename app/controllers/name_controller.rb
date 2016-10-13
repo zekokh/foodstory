91e3d6e88a52fbb15b9709302ab6ad5480cb39b6
zekokh <heroin160892>
2016-10-13 21:39:13 +0300

    init heroku

--- app/controllers/name_controller.rb
+++ app/controllers/name_controller.rb
@@ -1,6 +1,6 @@
 class NameController < ApplicationController
   def show
-    @custom = Custom.where("status = 3").order(:created_at).find(8)
-    @redy = Custom.where("status = 2").order(:created_at).find(8)
+    @custom = Custom.where("status = 3").order(created_at: :desc).take(8)
+    @redy = Custom.where("status = 2").order(created_at: :asc).take(8)
   end
 end
